173fa37f982fe418de66fbbbb603388257479d7c
Henryot <adinoyiotaru@gmail.com>
2023-10-10 00:49:03 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,3 +1,9 @@
-for i in range(10):
-	for j in range(i + 1, 10):
-		print("{}{}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
+#!/usr/bin/python3
+
+def print_combinations():
+    for i in range(10):
+        for j in range(i + 1, 10):
+            print("{:d}{:d}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
+
+if __name__ == "__main__":
+    print_combinations()
