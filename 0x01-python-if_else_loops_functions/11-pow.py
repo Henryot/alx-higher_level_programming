549b217a8c853337e99f9a5285901a5368203d00
Henryot <adinoyiotaru@gmail.com>
2023-10-10 01:36:03 -0700

    11-main.py

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    """Compute a to the power of b and return the value."""
+    return a ** b
+
+if __name__ == "__main__":
+    # Your test code here
+    pass
