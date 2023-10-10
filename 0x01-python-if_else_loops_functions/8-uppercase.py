90ac606e18bf8dee817c71b8184c135f739993b8
Henryot <adinoyiotaru@gmail.com>
2023-10-10 01:12:03 -0700

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    result = ""
+    for char in str:
+        if 97 <= ord(char) <= 122:
+            result += chr(ord(char) - 32)
+        else:
+            result += char
+    print("{}".format(result))
+
+if __name__ == "__main__":
+    # Your test code here
+    pass
+
