ea980ef8030580707c546aeaeee30314941dd038
Henryot <adinoyiotaru@gmail.com>
2023-10-22 08:18:27 -0700

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,7 @@
 
 def search_replace(my_list, search, replace):
     """
-    This function replaces all occurrences of an element by another in a new list.
+    This function replaces all occurrences of an element
+    by another in a new list.
     """
     return [replace if x == search else x for x in my_list]
