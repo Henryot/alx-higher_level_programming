92870c3c7c2e8ef394953cd58e96ce0576563dfe
Henryot <adinoyiotaru@gmail.com>
2023-10-22 13:03:08 -0700

    10 b3est score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    """
+    This function returns a key with the biggest integer value.
+    """
+    if not a_dictionary or len(a_dictionary) == 0:
+        return None
+    return max(a_dictionary, key=a_dictionary.get)
